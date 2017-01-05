651a98c12f46d7bae2ed924da59149c1f85ebbd8
Robert deMallac <demallac@gmail.com>
2017-01-05 08:45:43 -0800

    First commit

---
+++ src/main/java/com/grs/IndigoApplication.java
@@ -0,0 +1,12 @@
+package com.grs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndigoApplication.class, args);
+	}
+}
